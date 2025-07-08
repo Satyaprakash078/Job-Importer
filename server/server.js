55d4c2268fe33e66c2c38c054dde4a4c125c26f1
Satyaprakash078 <satyaprakashjena235@gmail.com>
2025-07-09 00:37:41 +0530

    Added cron job to fetch jobs every 10 minutes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import cron from 'node-cron'; 
 import { fetchAndQueueJobs } from './services/fetchJobs.service.js';
 import jobRoutes from './routers/jobs.routes.js'; 
 dotenv.config();
@@ -21,10 +22,19 @@ const startServer = async () => {
     app.listen(process.env.PORT, () => {
       console.log(`🚀 Server running on port ${process.env.PORT}`);
     });
+
+    
+    await fetchAndQueueJobs();
+
+    // Cron: run every hour (at minute 0)
+    cron.schedule('*/10 * * * *', async () => {
+     console.log('Cron Triggered: Fetching and queuing jobs...');
+      await fetchAndQueueJobs();
+    });
+
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
   }
 };
 
 startServer();
-await fetchAndQueueJobs();
